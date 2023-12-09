55dd1cae41fccc2083d9aab5e314321164a4c8f3
GH <130484247+vvzsf@users.noreply.github.com>
2023-12-09 23:04:25 +0530

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,20 +1,5 @@
-from telegram.ext import Updater, CommandHandler
-from Handler import start, set_thumbnail, handle_upload, handle_download
-from queue import Queue  # Import the Queue class
+from pyrogram import Client
+from Handler import app
 
-def main():
-    update_queue = Queue()  # Creating a Queue instance
-    updater = Updater("6625459474:AAEWMFsJYWg8QDb5aO56BRUvS4Mpul98TF8", update_queue=update_queue)
-
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("thumbnail", set_thumbnail))
-    dp.add_handler(CommandHandler("upload", handle_upload))
-    dp.add_handler(CommandHandler("dl", handle_download))
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app.start()
