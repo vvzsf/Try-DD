b6ba86327e87377ccd901a743e5494269457c551
GH <130484247+vvzsf@users.noreply.github.com>
2023-12-09 19:14:04 +0530

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,8 +1,9 @@
 from telegram.ext import Updater, CommandHandler
 from Handler import start, set_thumbnail, handle_upload, handle_download
+from queue import Queue  # Import the Queue class
 
 def main():
-    update_queue = Queue()
+    update_queue = Queue()  # Example of creating a Queue instance
     updater = Updater("6625459474:AAEWMFsJYWg8QDb5aO56BRUvS4Mpul98TF8", update_queue)
     dp = updater.dispatcher
 
