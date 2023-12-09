25f80d5b9b3f69729360fd24c9144902614a1a2e
GH <130484247+vvzsf@users.noreply.github.com>
2023-12-09 18:50:37 +0530

    Create Start.py

---
+++ Start.py
@@ -0,0 +1,17 @@
+from telegram.ext import Updater, CommandHandler
+from Handler import start, set_thumbnail, handle_upload, handle_download
+
+def main():
+    updater = Updater("6625459474:AAEWMFsJYWg8QDb5aO56BRUvS4Mpul98TF8")
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("thumbnail", set_thumbnail))
+    dp.add_handler(CommandHandler("upload", handle_upload))
+    dp.add_handler(CommandHandler("dl", handle_download))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main(
