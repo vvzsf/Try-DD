53ed727e13cd4b14f7929f0c94a9ea7bb73c0125
GH <130484247+vvzsf@users.noreply.github.com>
2023-12-10 13:21:37 +0530

    Update Handler.py

--- Handler.py
+++ Handler.py
@@ -1,6 +1,14 @@
 from pyrogram import Client, filters
+import os
+from pyrogram import Client
 
-app = Client("my_bot")
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
+
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+# Add your command handlers and bot logic here
 
 # Command to set a custom thumbnail
 @app.on_message(filters.command("thumbnail"))
