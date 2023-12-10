a96a789c8b9fd7d79752fd32c364d175153214cf
GH <130484247+vvzsf@users.noreply.github.com>
2023-12-10 13:34:01 +0530

    Update Handler.py

--- Handler.py
+++ Handler.py
@@ -1,6 +1,5 @@
 from pyrogram import Client, filters
 import os
-from pyrogram import Client
 
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
@@ -8,7 +7,18 @@ bot_token = os.environ.get("BOT_TOKEN")
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-# Add your command handlers and bot logic here
+# Function to handle the welcome message
+@app.on_chat_members_added()
+def welcome(client, message):
+    chat_id = message.chat.id
+    new_members = message.new_chat_members
+
+    for member in new_members:
+        username = member.username
+        welcome_message = f"Welcome, @{username}! Thanks for joining."
+
+        # Send welcome message
+        app.send_message(chat_id, welcome_message)
 
 # Command to set a custom thumbnail
 @app.on_message(filters.command("thumbnail"))
